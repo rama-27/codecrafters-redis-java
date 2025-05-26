4b59eb645fffa439247ee4c65208ed9f657ef57a
rama-27 <ramarajumantena231@gmail.com>
2025-05-26 16:46:41 +0530

    refactored threads to EventLoop Ig

--- src/main/java/dev/rama27/Main.java
+++ src/main/java/dev/rama27/Main.java
@@ -1,6 +1,5 @@
 package dev.rama27;
 import dev.rama27.components.NioServer;
-import dev.rama27.components.TcpServer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
@@ -8,11 +7,7 @@ import java.io.IOException;
 public class Main {
   public static void main(String[] args) throws IOException {
     System.out.println("Logs from your program will appear here!");
-      AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(Appconfig.class);
-
-//      TcpServer app=context.getBean(TcpServer.class);
-//      app.startServer();
-
+    AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(Appconfig.class);
     NioServer app=context.getBean(NioServer.class);
     app.startServer();
         
